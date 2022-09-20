59c13367f22aa145450903de6880b49e6f673c2c
Luis Cunha <luisfelipebc@outlook.com>
2022-09-20 08:42:53 -0300

    update project

--- code/methodology.R
+++ code/methodology.R
@@ -4,16 +4,19 @@ library(DiagrammeR)
 grViz("
       digraph {
       
-      graph[layout = dot, rankdir = LR]
+      graph[layout = dot, rankdir = TD]
 
-      input [label = 'NUI \n data set', shape = parallelogram]
-      step1 [label = 'ensemble modelling\n(k-modes with\ndifferent seeds)', shape = box]
-      step2 [label = 'consensus cluster\n(relabelling and\nmajority voting)', shape = box]
-      output [label = 'NUI \n typologies', shape = parallelogram]
-
-      input -> step1 -> step2 -> output
+      input [label = 'NUI dataset', shape = parallelogram]
+      step1 [label = '(1) Exploratory data analysis \n to choose the number of clusters', shape = box]
+      step2 [label = '(2) Clustering ensemble model \n (k-modes and consensus cluster)', shape = box]
+      output [label = 'NUI typologies', shape = parallelogram]
+      step3 [label = '(3) Descriptive and \n comparative analysis', shape = sdl_call]
+      
+      {rank = same; step1}
+      {rank = same; input; step2; step3; output}
+      
+      input -> step1 -> step2
+      input -> step2 -> output -> step3
       
       }
       ")
-
-#step2 [label = 'cluster correspondence \n (flatten and relabel)', shape = box]
